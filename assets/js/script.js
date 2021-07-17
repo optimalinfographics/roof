92308b5bfc4772fa263ef01164c29c1d026b428f
noaholatoye <noaholatoye@gmail.com>
2021-07-17 15:01:19 +0100

    Added Carousel Dot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
 let slideIndex = 0;
+const Carousel = document.getElementsByClassName("carousel")[0];
+const Slides = document.getElementsByClassName("carousel-box");
+const Dot = document.getElementsByClassName("carousel-dot")[0];
+const DotSpan = document.getElementsByClassName("dot");
+
+
+const ShowDot = () =>{
+
+    for (let i = 0; i < Slides.length; i++) {
+        let dot_span = document.createElement('SPAN');
+        dot_span.classList.add('dot')
+        Dot.append(dot_span);
+    }  
+}
+ShowDot()
+
 
 const showSlides = () =>{
-    const Slides = document.getElementsByClassName("carousel-box");
     for (let i = 0; i < Slides.length; i++) {
-        let slides = Slides[i]
-        slides.style.display = "none"; 
+        Slides[i].style.display = "none"; 
+        DotSpan[i].classList.remove('active');
     }   
     if (slideIndex >= Slides.length){
         slideIndex = 0;
     }
     Slides[slideIndex].style.display = "block";
+    DotSpan[slideIndex].classList.add('active');
     slideIndex++;
     
   setTimeout(showSlides, 4000); // Change image every 2 seconds
